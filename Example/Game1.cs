7b5781dafad4d34dab4e5b0532ea63c946ac5bd2
Dean Ellis <dellis1972@googlemail.com>
2026-02-15 11:09:38 +0000

    Updated example

--- Example/Game1.cs
+++ Example/Game1.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Reflection;
 using System.Runtime.InteropServices.JavaScript;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
@@ -13,7 +14,7 @@ public class Game1 : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
-    private Texture2D _texture;
+    private Texture2D _texture, _blankTexture;
     private SoundEffectInstance _soundEffect;
     private RenderTarget2D _renderTarget;
 
@@ -29,6 +30,7 @@ public class Game1 : Game
     protected override void Initialize()
     {
         base.Initialize();
+        Window.AllowUserResizing = true;
     }
 
     protected override void LoadContent()
@@ -36,53 +38,54 @@ public class Game1 : Game
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
         _texture = Content.Load<Texture2D>("test");
+        _blankTexture = new Texture2D(GraphicsDevice, 1, 1);
+        _blankTexture.SetData(new[] { Color.White });
 
         _soundEffect = Content.Load<SoundEffect>("testsound").CreateInstance();
-        _soundEffect.IsLooped = true;
-        _soundEffect.Play();
+        //_soundEffect.IsLooped = true;
+        //_soundEffect.Play();
 
         _renderTarget = new RenderTarget2D(GraphicsDevice, 200, 200);
     }
 
-    int x, y = 10;
+    int x, y = 0;
+    int h = 200;
 
     protected override void Update(GameTime gameTime)
     {
         var keyboardState = Keyboard.GetState();
-        if (keyboardState.IsKeyDown(Keys.Left))
-            x--;
-        if (keyboardState.IsKeyDown(Keys.Right))
-            x++;
+        if (keyboardState.IsKeyDown(Keys.Escape))
+            Exit();
+        if (keyboardState.IsKeyDown(Keys.S))
+            h-=10;
+        if (keyboardState.IsKeyDown(Keys.W))
+            h+=10;
         if (keyboardState.IsKeyDown(Keys.Up))
-            y--;
+            y-=10;
         if (keyboardState.IsKeyDown(Keys.Down))
-            y++;
+            y+=10;
         base.Update(gameTime);
     }
 
     protected override void Draw(GameTime gameTime)
     {
         GraphicsDevice.SetRenderTarget(_renderTarget);
-        GraphicsDevice.Clear(Color.MonoGameOrange);
+        GraphicsDevice.Clear(Color.Green);
         GraphicsDevice.SetRenderTarget(null);
-
-        var oldViewport = GraphicsDevice.Viewport;
-
-        GraphicsDevice.Viewport = new Viewport(0, 0, 10, 10);
-
         GraphicsDevice.Clear(Color.CornflowerBlue);
-
-        GraphicsDevice.Viewport = oldViewport;
 
         // GraphicsDevice.SamplerStates[0] = SamplerState.LinearClamp;
         // GraphicsDevice.BlendState = BlendState.Opaque;
         // GraphicsDevice.DepthStencilState = DepthStencilState.None;
         // GraphicsDevice.RasterizerState = RasterizerState.CullNone;
+        var vp = GraphicsDevice.Viewport;
 
         _spriteBatch.Begin();
         // Draw your game objects here
-        _spriteBatch.Draw(_texture, new Rectangle(x, y, 60, 60), Color.White);
-        //_spriteBatch.Draw(_renderTarget, new Rectangle(120, 20, 100, 60), Color.White);
+        _spriteBatch.Draw(_blankTexture, new Rectangle(vp.X, vp.Y, vp.Width, vp.Height), Color.Red);
+        _spriteBatch.Draw(_texture, new Rectangle(x, 10, 60, 60), Color.White);
+         _spriteBatch.Draw(_texture, new Rectangle(320-60, h-60, 60, 60), Color.White);
+        _spriteBatch.Draw(_renderTarget, new Rectangle(120, 20, 40, 40), Color.White);
         _spriteBatch.End();
 
         base.Draw(gameTime);
@@ -106,6 +109,8 @@ public static class Program
         using (var game = new Game1())
         {
             Console.WriteLine("Running Game1");
+            //for (int i = 0; i < 5; i++)
+            //    game.RunOneFrame();
             game.Run();
 #if MG_Web
             Console.WriteLine("Run returned now yielding to JS event loop");
